1a088df6d2773521619e6a2eaa2e538f40733238
Evan Fu <594380908@qq.com>
2020-12-30 17:23:56 +0800

    Update login20.sh

--- login20.sh
+++ login20.sh
@@ -2,6 +2,7 @@
 
 set timeout 30
 spawn ssh -p [lindex $1] [lindex $2]@[lindex $3]
+#spawn ssh -p [lindex $argv 0] [lindex $argv 1]@[lindex $argv 2]  #mac 版
 expect {
         "(yes/no)?"
         {send "yes\n";exp_continue}
